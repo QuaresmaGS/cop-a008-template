35d3fc194462cfa5ee904f6350f587f12ef14c7d
Gabriel <gabrielssquaresma@gmail.com>
2022-07-21 00:22:18 -0300

    atividade js. Lógico, relacional aritimético

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,20 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+
+console.log('exercicio 1')
+
+const pR1 = prompt('Qual o sua idade?');
+const pR2 = prompt('E a idade do seu melhor amigo?');
+console.log(pR1 > pR2);
+
+console.log('A sua idade é maior que a do seu amigo?' , pR1 > pR2);
+console.log('a diferença de idade é de' , pR1 - pR2, 'ano(s)');
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d
+(b && a) && !d
+a || 
